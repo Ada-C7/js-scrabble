5760c86982d917c36367b0c8befe75ee6941e348
Ann Dai <gofarann@gmail.com>
2017-05-16 13:31:43 -0700

    created hash object for scoreChart

--- scrabble.js
+++ scrabble.js
@@ -5,4 +5,30 @@ Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
 
+Scrabble.prototype = {
+  score: function(word){},
+  highestScoreFrom: function(arrayOfWords){}
+}
+
 module.exports = Scrabble;
+var scoreChart = new Object
+
+  scoreChart[1] = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
+  scoreChart[2] = ['D', 'G'];
+  scoreChart[3] = ['3', 'B', 'C', 'M', 'P'];
+  scoreChart[4] = ['F', 'H', 'V', 'W', 'Y'];
+  scoreChart[5] = ['K'];
+  scoreChart[8] = ['J', 'X'];
+  scoreChart[10] = ['Q', 'Z']
+
+var score = function(word) {
+  for(var i = 0, len = word.length; i < len; i++)
+
+    Object.keys(scoreChart).forEach(function (key) {
+
+      var value = scoreChart[key];
+      if value.includes(str[i])
+        console.log(key);
+        })
+      )
+    }
