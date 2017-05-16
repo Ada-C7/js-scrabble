ca1b9508e73ad0c694900b0ae069577d40dbcca8
Tamiko Terada <tamikodevo@gmail.com>
2017-05-16 15:15:12 -0700

    add prototype function to score word

--- scrabble.js
+++ scrabble.js
@@ -1,12 +1,38 @@
-//constructor
+// Constructor - Scrabble module
 var Scrabble = function() {
 };
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+// Prototype - function to score a single word
+Scrabble.prototype.score = function(word) {
+
+  var scoring_hash = {
+    aeioulnrst: 1,
+    dg: 2,
+    bcmp: 3,
+    fhvwy: 4,
+    k: 5,
+    kx: 8,
+    qz: 10,
+  };
+
+  var score = 0;
+  // iterate over each char of input word
+  for (var i = 0; i < word.length; i++) {
+    var letter = word[i];
+    // iterate over each key of hash
+    for (var key in scoring_hash) {
+      if (key.includes(letter)) {
+        score += scoring_hash[key];
+      }
+    }
+  }
+  return score;
 };
 
-module.exports = Scrabble;
-var my_game = new Scrabble()
-console.log(my_game.helloWorld());
+module.exports = Scrabble; // starter code
+
+// initiate new Scrabble object
+var my_game = new Scrabble();
+
+// print out results of single word score
+console.log(my_game.score("zzz"));
