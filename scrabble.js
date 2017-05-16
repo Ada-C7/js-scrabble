0b3ba00bbd7b11efd68360df38b493ef28e6ce08
Brenna <bcmdarroch@gmail.com>
2017-05-16 16:17:20 -0700

    scoring works, started highestScore

--- scrabble.js
+++ scrabble.js
@@ -1,74 +1,94 @@
 var Scrabble = function() {
-  var letter_values = {
-    a: 1,
-    b: 3,
-    c: 3,
-    d: 2,
-    e: 1,
-    f: 4,
-    g: 2,
-    h: 4,
-    i: 1,
-    j: 8,
-    k: 5,
-    l: 1,
-    m: 3,
-    n: 1,
-    o: 1,
-    p: 3,
-    q: 10,
-    r: 1,
-    s: 1,
-    t: 1,
-    u: 1,
-    v: 4,
-    w: 4,
-    x: 8,
-    y: 4,
-    z: 10
+  this.letter_values = {
+    "a": 1,
+    "b": 3,
+    "c": 3,
+    "d": 2,
+    "e": 1,
+    "f": 4,
+    "g": 2,
+    "h": 4,
+    "i": 1,
+    "j": 8,
+    "k": 5,
+    "l": 1,
+    "m": 3,
+    "n": 1,
+    "o": 1,
+    "p": 3,
+    "q": 10,
+    "r": 1,
+    "s": 1,
+    "t": 1,
+    "u": 1,
+    "v": 4,
+    "w": 4,
+    "x": 8,
+    "y": 4,
+    "z": 10
   };
 };
 
-Scrabble.prototype.score = function(input_word) {
-  // regex, raise errors
-  if (/^[a-zA-Z]+$/.test(input_word)) {
+Scrabble.prototype = {
+  score: function(input_word) {
+    // regex, raise errors
+    if (/^[a-zA-Z]+$/.test(input_word)) {
 
-    // case insensitive
-    var word = input_word.toLowerCase();
+      // case insensitive
+      var word = input_word.toLowerCase();
 
-    // 7 letter word bonus
-    var word_score = (word.length == 7) ? 50 : 0;
+      // 7 letter word bonus
+      var word_score = (word.length == 7) ? 50 : 0;
 
-    // isolate letters
-    for (var i = 0; i < word.length; i++) {
-      // assign scores to letters
-      var letter_score = this.letter_values[word.charAt(i)];
-      word_score += letter_score;
+      // isolate letters
+      for (var i = 0; i < word.length; i++) {
+        // assign scores to letters
+        word_score += this.letter_values[word.charAt(i)];
+      }
+
+      // return total score
+      return word_score;
+
+    } else {
+      throw "Sorry, invalid input!";
     }
+  },
 
-    // return total score
-    return word_score;
+  // highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
+  highestScoreFrom: function(arrayOfWords) {
+    arrayOfWords.forEach (function(num) {
+      if (/^[a-zA-Z]+$/.test(input_word)) {
+        // iterate through words array
 
-  } else {
-    throw "Sorry, invalid input!";
+        // scores array var
+
+        // for each word, return score w score(word) (map to score array)
+
+        // use max of scores array to return winner
+
+
+
+
+      } else {
+        throw "Sorry, invalid input! Please enter a word.";
+      }
+    });
   }
+
+  // tiebreaker method
+  // if tied for top score, winner is shortest length
+  // seven letter words get 50 points, if tied for top score, winner is 7 letter word (over shorter)
+  // if multiple ties of same length, winner is first word
 };
 
 var myScrabble = new Scrabble();
+
+console.log(myScrabble.score("ZZzzZZ")); // 60
+console.log(myScrabble.score("niiice")); // 8
 // console.log(myScrabble.score(456));
-console.log(myScrabble.score("niiice"));
 
 
-// highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
-// iterate through words array
-// scores array var
-// for each word, return score w score(word) (map to score array)
-// return max of scores array
 
-// tiebreaker method
-// if tied for top score, winner is shortest length
-// seven letter words get 50 points, if tied for top score, winner is 7 letter word (over shorter)
-// if multiple ties of same length, winner is first word
 
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
