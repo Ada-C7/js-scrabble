fc2580cf45fd0ca08e578b12a95e80449a8ca5ea
Theresa Manney <theresa.m.17@gmail.com>
2017-05-16 13:46:00 -0700

    basline complete, hello world is working

--- scrabble.js
+++ scrabble.js
@@ -4,5 +4,17 @@ var Scrabble = function() {};
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
+var test = new Scrabble();
+console.log(test.helloWorld());
 
 module.exports = Scrabble;
+
+
+// 1. start with ruby code and go from there
+// OR 2. start from scratch to see how far you have come and to see if you go about solving in a different way
+// will have to look up new syntax of course sense this is a new language
+
+// TWO SECTIONS they could be separated in different modles but thats not important right now as long as they are obvious in different sections
+// Game
+
+// User
