c415d66fc36a30274aaf4ad0750ab775ea6b9f8c
Tamiko Terada <tamikodevo@gmail.com>
2017-05-16 16:38:16 -0700

    add function to find highest scoring word from array

--- scrabble.js
+++ scrabble.js
@@ -29,6 +29,38 @@ Scrabble.prototype.score = function(word) {
   return score;
 };
 
+// Prototype - function to find max score word in array
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+
+  var high_score = this.score(arrayOfWords[0]);
+
+  // iterate over array of input words to find max score
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    var word_score = this.score(arrayOfWords[i]);
+    if ( word_score > high_score) {
+      high_score = word_score;
+    }
+  };
+
+  // add words with high_score into an array
+  var max_words = [];
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    if (this.score(arrayOfWords[i]) === high_score) {
+      max_words.push(arrayOfWords[i]);
+    }
+  }
+
+  // return the word with shortest length
+  var shortest_word = max_words[0];
+  for (var i = 0; i < max_words.length; i++)
+    if (max_words[i].length < shortest_word.length) {
+      shortest_word = max_words[i];
+    };
+  return shortest_word;
+};
+
+var max_score = []; // array to hold max words
+
 module.exports = Scrabble; // starter code
 
 // initiate new Scrabble object
@@ -36,3 +68,4 @@ var my_game = new Scrabble();
 
 // print out results of single word score
 console.log(my_game.score("zzz"));
+console.log(my_game.highestScoreFrom(["aaa", "zzj", "zzx"]));
