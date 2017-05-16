19d41cd04b6dfc9bda69af657676c177a258aba5
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-16 16:44:06 -0700

    one idea down, but don't know if chart place good

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,48 @@
-var Scrabble = function() {};
+// Given sample
+// var Scrabble = function() {};
+//
+// // YOUR CODE HERE
+// Scrabble.prototype.helloWorld = function() {
+//   return 'hello world!';
+// };
+//
+// module.exports = Scrabble;
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
-};
+//var Scrabble = function(/*word*/) {
+  /*this.scoreChart = {
+      1: [
+         "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+       ],
+      2: [
+        "D", "G"
+       ],
+       3: [
+         "B", "C", "M", "P"
+       ],
+      4: [
+         "F", "H", "V", "W", "Y"
+       ],
+       5: [
+         "K"
+       ],
+       8: [
+         "J", "X"
+       ],
+       10: [
+         "Q", "Z"
+       ]
+  };*/
+  //this.word = word;
+//};
 
-module.exports = Scrabble;
+//Scrabble.prototype = {
+  // score: function(word) {
+  //   for (var letter of word) {
+  //     for (var score in scoreChart) {
+  //       if (object.hasOwnProperty(variable)) {
+  //
+  //       }
+  //     }
+  //   }
+  // }
+//};
