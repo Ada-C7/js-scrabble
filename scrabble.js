c175a325c84a390d8d38c7101e9a2134824e74c8
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-16 14:14:46 -0700

    Created score word method in Scrabble prototype

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,50 @@
 var Scrabble = function() {};
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+
+
+Scrabble.prototype = {
+  score: function(word) {
+    var values = {
+      a: 1,
+      e: 1,
+      i: 1,
+      o: 1,
+      u: 1,
+      l: 1,
+      n: 1,
+      r: 1,
+      s: 1,
+      t: 1,
+      d: 2,
+      g: 2,
+      b: 3,
+      c: 3,
+      m: 3,
+      p: 3,
+      f: 4,
+      h: 4,
+      v: 4,
+      w: 4,
+      y: 4,
+      k: 5,
+      j: 8,
+      x: 8,
+      q: 10,
+      z: 10
+    };
+    var score = 0;
+
+    word = word.toLowerCase();
+
+    for (i = 0; i < word.length; i++) {
+      letter = word[i];
+      score += values[letter];
+    }
+    return score;
+  }
 };
+
+var test = new Scrabble();
+console.log(test.score("TEST"));
 
 module.exports = Scrabble;
