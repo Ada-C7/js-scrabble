fff20fae952864675e7ae0cd53941192d2da0ab5
janicewilson <janice.wilson@mac.com>
2017-05-16 15:43:09 -0700

    score function has been created.

--- scrabble.js
+++ scrabble.js
@@ -29,4 +29,59 @@ var scoreChart = {
      Z: 10
 };
 
+var score = function(word) {
+
+     if (/[^a-z]/i.test(word) || !(word)) {
+          return 'This serves as a gentle reminder that you\'re playing Scrabble. Letters only, please.';
+     } else {
+          var wordArray = word.toUpperCase().split("");
+     }
+
+     var scoreArray = [],
+          total = 0,
+          packaged = [];
+
+     wordArray.forEach(function(letter){
+          scoreArray.push(scoreChart[letter]);
+     });
+
+     scoreArray.forEach(function(score){
+          total += score;
+     });
+
+     if (scoreArray.length == 7) {
+          total += 50;
+     }
+
+     packaged.push(wordArray, total);
+     return packaged;
+};
+
+var selectHighestScore = function(wordArray) {
+      highestScore = [[["a"],1]];
+
+     wordArray.forEach(function(word){
+          var packaged =[];
+          packaged = score(word);
+          console.log(highestScore);
+
+          if (highestScore[0][1] == packaged[1]) {
+               highestScore.push(packaged);
+          } else if (highestScore[0][1] < packaged[1]) {
+               highestScore.splice(-1, 1)
+               highestScore.push(packaged);
+          }
+     });
+     return highestScore;
+};
+
+// highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
+// Note that it’s better to use fewer tiles, so if the top score is tied between multiple words, pick the one with the fewest letters.
+// Note that there is a bonus (50 points) for using all seven letters. If the top score is tied between multiple words and one used all seven letters, choose the one with seven letters over the one with fewer tiles.
+// If the there are multiple words that are the same score and same length, pick the first one in supplied list.
+wordArray = ['empire', 'strikes', 'back', 'return', 'of', 'the', 'jedi', 'zawisto', 'zawista']
+
+console.log(selectHighestScore(wordArray));
+
+
 module.exports = Scrabble;
