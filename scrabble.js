60aa7cbba2577da1c951894a2ac2ab42ff3f310c
Janice Lichtman <jalichtman@gmail.com>
2017-05-16 13:55:45 -0700

    wrote basic word score function

--- scrabble.js
+++ scrabble.js
@@ -6,3 +6,47 @@ Scrabble.prototype.helloWorld = function() {
 };
 
 module.exports = Scrabble;
+
+
+
+var score = function(word){
+  total_score = 0;
+  word.forEach(function(letter) {
+    total_score += scoringChart[letter];
+  });
+  return total_score;
+};
+
+
+
+
+var scoringChart = {
+  "a" : 1,
+  "b" : 3,
+  "c" : 3,
+  "d" : 2,
+  "e" : 1,
+  "f" : 4,
+  "g" : 2,
+  "h" : 4,
+  "i" : 1,
+  "j" : 8,
+  "k" : 5,
+  "l" : 1,
+  "m" : 3,
+  "n" : 1,
+  "o" : 1,
+  "p" : 3,
+  "q" : 10,
+  "r" : 1,
+  "s" : 1,
+  "t" : 1,
+  "u" : 1,
+  "v" : 4,
+  "w" : 4,
+  "x" : 8,
+  "y" : 4,
+  "z" : 10,
+};
+
+console.log(scoringChart.z);
