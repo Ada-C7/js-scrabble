a23cb0ca3361c57a942fb48608a749438190d5b8
Anacelia Gillette <acgillette426@gmail.com>
2017-05-16 14:42:24 -0700

    Finished score word and score array of words functionality

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,49 @@
-var Scrabble = function() {};
+
+
+var Scrabble = function() {
+  this.scoreCard = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q","Z"]
+  };
+
+  this.scores = [];
+};
 
 // YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+Scrabble.prototype = {
+  score: function(word) {
+    var score = 0;
+    word = word.toUpperCase();
+    var letters = word.split("");
+    letters.forEach(function(letter){
+      for(var k in this.scoreCard) {
+        if (this.scoreCard[k].indexOf(letter) !== -1) {
+          score += parseInt(k);
+        }
+      }
+    }, this);
+    return score;
+  },
+  highestScoreFrom: function(arrayOfWords) {
+    this.scores = [];
+    arrayOfWords.forEach(function(word) {
+      var score = this.score(word);
+      this.scores.push(score);
+    }, this);
+
+    var maxIndex = this.scores.indexOf(Math.max(...this.scores));
+    return arrayOfWords[maxIndex];
+  }
 };
+
+myScrabble = new Scrabble();
+console.log(myScrabble.scoreCard);
+console.log(myScrabble.score("superfresh"));
+console.log(myScrabble.highestScoreFrom(["hello", "world", "elephant"]));
 
 module.exports = Scrabble;
