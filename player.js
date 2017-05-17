b112492f48effe18f6965b457fc995c7fc46a566
Ting Wong <hellotingwong@gmail.com>
2017-05-17 16:06:32 -0700

    I'm sorry, I've totally forgotten to commit - added Player class

--- player.js
+++ player.js
@@ -1,2 +1,38 @@
 
 var Scrabble = require('./scrabble');
+
+var Player = function(name) {
+  this.name = name;
+  this.plays = [];
+};
+
+Player.prototype.play = function(word) {
+  this.plays.push(word);
+  return this.plays;
+};
+
+Player.prototype.totalScore = function() {
+  var currentScore = 0
+  // var self = this
+  this.plays.forEach(function(word) {
+    currentScore += Scrabble.wordScore(word);
+  });
+  return currentScore;
+};
+
+Player.prototype.hasWon = function() {
+  return (this.totalScore() > 100);
+};
+
+Player.prototype.highestScoringWord = function() {
+  return Scrabble.highestScoreFrom(this.plays);
+};
+
+var ting = new Player("ting");
+console.log(ting.play("hello"));
+console.log(ting.play("xylophone"));
+console.log(ting.play("zzzzzzzzz"));
+console.log(ting.totalScore());
+console.log(ting.hasWon());
+console.log(ting.play("apple"));
+console.log(ting.highestScoringWord());
--- scrabble.js
+++ scrabble.js
@@ -5,47 +5,45 @@ const SCORECHART = {
   "Q": 10, "Z": 10
 };
 
-var Scrabble = function() {
-};
-
-Scrabble.prototype.wordScore = function(word) {
-  var wordArray = word.toUpperCase().split("");
-  var wordScore = 0;
-  for (var i = 0; i < wordArray.length; i++) {
-    letter = wordArray[i];
-    wordScore += SCORECHART[letter];
-  }
-  if (wordArray.length == 7) {
-    wordScore += 50;
-  }
-  return wordScore;
-};
-
-Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-  var self = this;
-  var scoreHash = {};
-  var arrayOfWords = arrayOfWords;
-  var singleWordScore = 0;
-  var max_score = 0;
-  var max_word = 0;
-  for (var i = 0; i < arrayOfWords.length; i++) {
-    singleWordScore = self.wordScore(arrayOfWords[i]);
-    scoreHash[arrayOfWords[i]] = singleWordScore;
-  }
-  for (var key in scoreHash) {
-    if (scoreHash[key] > max_score) {
-      max_score = scoreHash[key];
-      max_word = key;
-    } else if (scoreHash[key] == max_score && key.length == 7 && max_word.length != 7) {
-      max_word = key;
-    } else if (scoreHash[key] == max_score && key.length < max_word.length && max_word.length != 7) {
-      max_word = key;
+var Scrabble = {
+  wordScore: function(word) {
+    var wordArray = word.toUpperCase().split("");
+    var wordScore = 0;
+    for (var i = 0; i < wordArray.length; i++) {
+      letter = wordArray[i];
+      wordScore += SCORECHART[letter];
     }
+    if (wordArray.length == 7) {
+      wordScore += 50;
+    }
+    return wordScore;
+  },
+  highestScoreFrom: function(arrayOfWords) {
+    var scoreHash = {};
+    var arrayOfWords = arrayOfWords;
+    var singleWordScore = 0;
+    var max_score = 0;
+    var max_word = 0;
+    for (var i = 0; i < arrayOfWords.length; i++) {
+      singleWordScore = this.wordScore(arrayOfWords[i]);
+      scoreHash[arrayOfWords[i]] = singleWordScore;
+    }
+    for (var key in scoreHash) {
+      if (scoreHash[key] > max_score) {
+        max_score = scoreHash[key];
+        max_word = key;
+      } else if (scoreHash[key] == max_score && key.length == 7 && max_word.length != 7) {
+        max_word = key;
+      } else if (scoreHash[key] == max_score && key.length < max_word.length && max_word.length != 7) {
+        max_word = key;
+      }
+    }
+    return max_word;
   }
-  return max_word;
 };
+
 
 module.exports = Scrabble;
 
-var play = new Scrabble;
-console.log(play.highestScoreFrom(["Hello","J", "AAAAAAD", "AAAAAAG", "HELLOQQQQQ", "ZZZZZZ"]));
+console.log(Scrabble.wordScore("Hello"));
+console.log(Scrabble.highestScoreFrom(["Hello","J", "AAAAAAD", "AAAAAAG", "HELLOQQQQQ", "ZZZZZZ"]));
