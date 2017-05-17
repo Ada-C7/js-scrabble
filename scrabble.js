d21254a9db0e382540c1d0abf465f426b98b6d88
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-17 15:36:51 -0700

    added comments to scrabble file

--- scrabble.js
+++ scrabble.js
@@ -1,3 +1,5 @@
+
+//// set up scrabble to be an object that contians a sub-object and scoring functions
 var Scrabble = {
 
   letterScores: letterScores = {
@@ -6,6 +8,8 @@ var Scrabble = {
     'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
   },
 
+  // this function will score a word - the word has already been tested in the player
+  // class so you know it is a good word
   score: function(word) {
     var total = 0;
     var letters = word.toUpperCase().split('');
@@ -19,6 +23,7 @@ var Scrabble = {
   },
 
 
+  // this function will find the highest scoring word or words if there is a tie
   highestScoreFrom: function (words) {
     var highestScoringWords = this._findHighestScoring(words);
     var highestWord = undefined;
@@ -33,6 +38,7 @@ var Scrabble = {
     return highestWord
   },
 
+// this is a helper function for highestScoreFrom
   _findHighestScoring: function(words) {
     var wordsWithScores = {};
     var highestScore = 0;
@@ -42,26 +48,27 @@ var Scrabble = {
       var score = Scrabble.score(word);
       wordsWithScores[word] = score;
 
-      // will determine max score
+      // will reassigne the highestScore
       if ( score > highestScore ){ highestScore = score; }
     });
 
-    // console.log(wordsWithScores);
-    //find any tie words - or singular words with the highest score
+
+    // finds any tie words - or will return an array with the highest scoring word
     var highestScoring = Object.keys(wordsWithScores).filter( function(word) {
       if ( wordsWithScores[word] === highestScore ) { return word; }
     });
 
-    // returns words with the highest score (ie tie - this will have multiple words - just one highest - only one word)
+    // returns word(s) with the highest score
     return highestScoring;
   },
 
-  // if work length is 7 it beats all other words
+  // a helper function for highestScoreFrom
   _breakTie: function(words){
-    // console.log(words);
     var winner = undefined;
     var length = 8;
 
+    // loops through the tie words - if length is 7 - that word is the winner
+    // else the shorest length word is the winner.
     words.forEach(function (word){
       if (word.length == 7 ) {
         winner = word;
@@ -76,8 +83,3 @@ var Scrabble = {
 }
 
 module.exports = Scrabble;
-// words = ["hellooo", "cateee", "zzzz"];
-// words = ["zzzzzz", "hellooo"]
-// words = ["cat", "cat"]
-// console.log(Scrabble.score("hello"));
-// console.log(Scrabble.highestScoreFrom(words));
