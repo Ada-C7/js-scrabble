aa29bcbe108ee1117cae21c3cfffd28cbdae5f74
Cara Comfort <cara.comfort10@gmail.com>
2017-05-17 13:28:58 -0700

    Implement methods to return highest scoring word and highest word score for player class

--- player.js
+++ player.js
@@ -17,8 +17,19 @@ Player.prototype.totalScore = function() {
 
 Player.prototype.hasWon = function() {
   return ( this.totalScore() >= 100 ) ? true : false;
-}
+};
+
+Player.prototype.highestScoringWord = function() {
+  return this.scrabble.highestScoreFrom(this.plays);
+};
+
+Player.prototype.highestWordScore = function() {
+  topWord = this.highestScoringWord();
+  return this.scrabble.score(topWord);
+};
 
 var my_player = new Player('Ada');
 console.log(my_player.totalScore());
 console.log(my_player.hasWon());
+console.log(my_player.highestScoringWord());
+console.log(my_player.highestWordScore());
