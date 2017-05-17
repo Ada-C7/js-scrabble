d99123d5fa34f2902e5eb668776488305f82783f
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-17 11:27:58 -0700

    total score function working

--- player.js
+++ player.js
@@ -6,15 +6,21 @@ var Player = function (name){
 };
 // play(word)
 Player.prototype.play = function(word){
-  //test word
+  //test word?
   //word needs to pushed into plays
-  //word needs to be scored?
-  //returns false if player has already won
   this.plays.push(word);
+  //returns false if player has already won
 };
 
 Player.prototype.totalScore = function(){
+  var totalScore = 0;
 
+  this.plays.forEach(function (word){
+    var points = Scrabble.score(word);
+    totalScore += points;
+  });
+
+  return totalScore;
 };
 
 Player.prototype.hasWon = function(){
@@ -34,8 +40,9 @@ Player.prototype.highestWordScore = function() {
 var player1 = new Player ("cynthia");
 console.log(player1.name);
 player1.play("hello");
-player1.play("cat");
-player1.play("dog");
-player1.play("xo");
+// player1.play("cat");
+// player1.play("dog");
+// player1.play("xo");
 player1.play("zoo");
 console.log(player1.plays);
+console.log(player1.totalScore());
