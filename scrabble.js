a1daa92d4440c173bc660859d91ff06529250723
Andrea Valliere <amvalliere@gmail.com>
2017-05-17 12:24:42 -0700

    Added constant SCORECHART, added prototype with method to score a word

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,39 @@
-var Scrabble = function() {};
+// constructor: like initialize - handles the things specific to an instance
+// prototype: holds information that is needed for all instances (score hash)
 
-// YOUR CODE HERE
+const SCORECHART = {
+  "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2,
+  "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, "K": 5, "J": 8, "X": 8,
+  "Q": 10, "Z": 10
+};
+
+// constructor
+var Scrabble = function() {
+
+};
+
+// prototypes
+
+// example prototype
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
 
+Scrabble.prototype.score = function(word) {
+
+  // make word into an array
+  var wordArray = word.toUpperCase().split("");
+  var wordScore = 0;
+  for (var i = 0; i < word.length; i += 1) {
+    letter = wordArray[i];
+    wordScore += SCORECHART[letter];
+  }
+  return wordScore;
+};
+
+
 module.exports = Scrabble;
+
+var game = new Scrabble();
+console.log(game.helloWorld());
+console.log(game.score("hello"));
