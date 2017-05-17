86e35957dc44eac39d0574bf935cc8b497d4e5fb
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-17 11:46:58 -0700

    score chart and game skeleton

--- scrabble.js
+++ scrabble.js
@@ -8,41 +8,65 @@
 //
 // module.exports = Scrabble;
 
-//var Scrabble = function(/*word*/) {
-  /*this.scoreChart = {
-      1: [
-         "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
-       ],
-      2: [
-        "D", "G"
-       ],
-       3: [
-         "B", "C", "M", "P"
-       ],
-      4: [
-         "F", "H", "V", "W", "Y"
-       ],
-       5: [
-         "K"
-       ],
-       8: [
-         "J", "X"
-       ],
-       10: [
-         "Q", "Z"
-       ]
-  };*/
-  //this.word = word;
-//};
+var scoreChart = {
+  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1,
+  'L' : 1, 'N' : 1, 'R' : 1, 'S' : 1, 'T' : 1,
+  'D' : 2, 'G' : 2,
+  'B' : 3, 'C' : 3, 'M' : 3, 'P' : 3,
+  'F' : 4, 'H' : 4, 'V' : 4, 'W' : 4, 'Y' : 4,
+  'K' : 5,
+  'J' : 8, 'X' : 8,
+  'Q' : 10, 'Z' : 10
+};
 
-//Scrabble.prototype = {
-  // score: function(word) {
-  //   for (var letter of word) {
-  //     for (var score in scoreChart) {
-  //       if (object.hasOwnProperty(variable)) {
-  //
-  //       }
-  //     }
-  //   }
-  // }
-//};
+// SCRABBLE
+// constroctor -- instance variables
+var Scrabble = function(input) {
+  this.word = input;
+  /* how to put in the score chart?
+  Before I thought I put it in here as an object
+  Now I have it as a hash*/
+};
+
+// iterators -- instance methods
+Scrabble.prototype.score = function() {
+
+};
+
+Scrabble.prototype.highestScoreFrom = function() {
+
+};
+
+// initializer
+module.exports = Scrabble;
+
+
+// Player
+// constroctor -- instance variables
+var Player = function(input) {
+  this.name = input;
+};
+
+// iterators -- instance methods
+Player.prototype.play = function() {
+
+};
+
+Player.prototype.totalScore = function() {
+
+};
+
+Player.prototype.hasWon = function() {
+
+};
+
+Player.prototype.highestScoringWord = function() {
+
+};
+
+Player.prototype.highestWordScore = function() {
+
+};
+
+// initializer
+module.exports = Player;
