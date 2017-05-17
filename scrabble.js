fbd7f0664e5a67bc6728312bb7b9b972f6a02e76
Janice Lichtman <jalichtman@gmail.com>
2017-05-16 23:51:36 -0700

    completed highest score function including tie handling

--- scrabble.js
+++ scrabble.js
@@ -14,20 +14,26 @@ var score = function(word){
   for(var i = 0; i < word.length; i++){
     total_score += scoringChart[word[i]];
   }
+  if (word.length == 7){
+    total_score +=50;
+    }
   return total_score;
 };
 
 
 var highestScoreFrom = function(arrayOfWords){
   return arrayOfWords.reduce(function(winner, element){
-      if (score(element)> score(winner)) {
+      if (score(element)> score(winner)){
         return element;
+      }else if ((score(element) == score(winner)) &&  (element.length == 7) && (winner.length != 7)){
+        return element;
+      }else if ((score(element) == score(winner)) &&  (element.length < winner.length) && (winner.length != 7)){
+      return element;
       }else{
         return winner;
       }
     }, "");
   };
-
 
 var scoringChart = {
   "a" : 1,
@@ -60,5 +66,15 @@ var scoringChart = {
 
 console.log(score('pizza'));
 console.log(score('coffee'));
+console.log(score('pikkkka'));
 
-console.log(highestScoreFrom(['pizza', 'coffee']));
+console.log(highestScoreFrom(['pizza', 'coffee', 'fizz','pikkkka', 'pizze']));
+console.log(score('qqqqqq'));
+console.log(score('aaaaaah'));
+
+
+console.log(highestScoreFrom(['pizza', 'coffee', 'fizz','aaaaaah', 'qqqqqq',  'pizze',]));
+
+
+
+console.log(highestScoreFrom(['pizza', 'coffee', 'fizz', 'qqqqqq',  'aaaaaah', 'pizze',]));
