6242930d8bdfa235dbb169249ea1fdb7aec707d5
Haby Randall <sow.haby@gmail.com>
2017-05-17 11:35:07 -0700

    Changed score chart from constant to local var

--- scrabble.js
+++ scrabble.js
@@ -1,43 +1,45 @@
-var Scrabble = function() {
-  SCORE_CHART = {
-      "A" : 1,
-      "E" : 1,
-      "I" : 1,
-      "O" : 1,
-      "U" : 1,
-      "L" : 1,
-      "N" : 1,
-      "R" : 1,
-      "S" : 1,
-      "T" : 1,
-      "D" : 2,
-      "G" : 2,
-      "B" : 3,
-      "C" : 3,
-      "M" : 3,
-      "P" : 3,
-      "F" : 4,
-      "H" : 4,
-      "V" : 4,
-      "W" : 4,
-      "Y" : 4,
-      "K" : 5,
-      "J" : 8,
-      "X" : 8,
-      "Q" : 10,
-      "Z" : 10
-    };
-};
+var Scrabble = function() {};
 
 
   // YOUR CODE HERE
 Scrabble.prototype.score = function(word) {
+
+  var scoreChart = {
+        "A" : 1,
+        "E" : 1,
+        "I" : 1,
+        "O" : 1,
+        "U" : 1,
+        "L" : 1,
+        "N" : 1,
+        "R" : 1,
+        "S" : 1,
+        "T" : 1,
+        "D" : 2,
+        "G" : 2,
+        "B" : 3,
+        "C" : 3,
+        "M" : 3,
+        "P" : 3,
+        "F" : 4,
+        "H" : 4,
+        "V" : 4,
+        "W" : 4,
+        "Y" : 4,
+        "K" : 5,
+        "J" : 8,
+        "X" : 8,
+        "Q" : 10,
+        "Z" : 10
+      };
+
     // return an array of letters in the word
     var letters = word.toUpperCase().split("");
 
   var scoreTotal = 0;
+
   letters.forEach(function (letter) {
-    scoreTotal += SCORE_CHART[letter];
+    scoreTotal += scoreChart[letter];
   });
   if (word.length >= 7) {
     scoreTotal += 50;
@@ -64,7 +66,7 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
 
 //apple: 9, banana: 8, pof: 8
 var game = new Scrabble();
-play = game.score("pof");
+play = game.score("bof");
 topScore = game.highestScoreFrom(["banana", "pof"]);
 console.log(play);
 console.log(topScore);
