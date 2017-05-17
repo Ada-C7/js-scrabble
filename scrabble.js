12d955c8776c6c1898d659c5901dd90718051b96
Theresa Manney <theresa.m.17@gmail.com>
2017-05-17 15:02:25 -0700

    Word will get a 50 point bonus if all 7 letters are used

--- scrabble.js
+++ scrabble.js
@@ -4,65 +4,87 @@ var Scrabble = function() {};
 
 /* ##### BASELINE #####
 Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+return 'hello world!';
 };
 var test = new Scrabble();
 console.log(test.helloWorld());
 ######################### */
 
 
-Scrabble.prototype = {
-  score: function(word) {
-    var one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
-    var two = ["D", "G"];
-    var three = ["B", "C", "M", "P"];
-    var four = ["F", "H", "V", "W", "Y"];
-    var five = ["K"];
-    var eight = ["J", "X"];
-    var ten = ["Q", "Z"];
+Scrabble.prototype.score = function(word) {
+  var one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
+  var two = ["D", "G"];
+  var three = ["B", "C", "M", "P"];
+  var four = ["F", "H", "V", "W", "Y"];
+  var five = ["K"];
+  var eight = ["J", "X"];
+  var ten = ["Q", "Z"];
 
-    var wordScore = 0;
-    var upcaseWord = word.toUpperCase();
+  var wordScore = 0;
+  var upcaseWord = word.toUpperCase();
 
-    // loop through each character
-    for (var i = 0; i < word.length; i++) {
-      // if else statments to assign each character a point value
-      if (one.includes(upcaseWord.charAt(i))) {
-        wordScore += 1;
-      } else if (two.includes(upcaseWord.charAt(i))){
-        wordScore += 2;
-      } else if (three.includes(upcaseWord.charAt(i))){
-        wordScore += 3;
-      } else if (four.includes(upcaseWord.charAt(i))){
-        wordScore += 4;
-      } else if (five.includes(upcaseWord.charAt(i))){
-        wordScore += 5;
-      } else if (eight.includes(upcaseWord.charAt(i))){
-        wordScore += 8;
-      } else if (ten.includes(upcaseWord.charAt(i))){
-        wordScore += 10;
-      } else {
-        // case where there is a non character value in word
-        // Add some sort of response or validations..
-      }
-
+  // loop through each character
+  for (var i = 0; i < word.length; i++) {
+    // if else statments to assign each character a point value
+    if (one.includes(upcaseWord.charAt(i))) {
+      wordScore += 1;
+    } else if (two.includes(upcaseWord.charAt(i))){
+      wordScore += 2;
+    } else if (three.includes(upcaseWord.charAt(i))){
+      wordScore += 3;
+    } else if (four.includes(upcaseWord.charAt(i))){
+      wordScore += 4;
+    } else if (five.includes(upcaseWord.charAt(i))){
+      wordScore += 5;
+    } else if (eight.includes(upcaseWord.charAt(i))){
+      wordScore += 8;
+    } else if (ten.includes(upcaseWord.charAt(i))){
+      wordScore += 10;
+    } else {
+      // case where there is a non character value in word
+      // Add some sort of response or validations..
     }
-    return wordScore;
-  },
-  highestScoreFrom: function(arrayOfWords){
-    // top score tied between two words? choose shortest word
-    // 50 pt bonus for using all seven letters
-    // multiple words what are same score and same length? pick the first one
+
   }
+  return wordScore;
 };
 
+Scrabble.prototype.highestScore = function(arrayOfWords) {
+  var winningWord = "";
+  var winningScore = 0;
 
-var game  = new Scrabble();
-console.log(game.score("adbfkjq"));
+  //MAIN//
+  // loop that will go through arrayOfWords
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    var word = arrayOfWords[i];
+    var score = this.score(word);
+
+    // 50 pt bonus for using all seven letters
+    if (word.length == 7) {
+      score += 50;
+    }
+
+    //conditionals that test
+    // top score tied between two words? choose shortest word
+    // multiple words what are same score and same length? pick the first one
+
+  //return winning word
+  return score;
+  }
+};
+// TESTS
+var testGame  = new Scrabble();
+
+// var wordsArray = ["Test", "buTTerfly", "caR"];
+var wordsArray = ["aaaaaaa"];
+// should return buTTerfly
+console.log(testGame.score("adbfkjq"));
 // test should = 4
+console.log(testGame.highestScore(wordsArray));
+
 module.exports = Scrabble;
 
-// User
+// PLAYER
 
 
 
