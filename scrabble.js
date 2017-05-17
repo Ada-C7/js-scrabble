8062ee6a568fc34c250ed980d978d107076fa12a
add2point71dots <alcland@gmail.com>
2017-05-17 15:19:34 -0700

    took out unnecessary variable in highestScoreFrom

--- scrabble.js
+++ scrabble.js
@@ -17,34 +17,31 @@ Scrabble.score = function(word) {
 
 Scrabble.highestScoreFrom = function(arrayOfWords) {
   var highScore = -1;
-  var highScoreWord;
-  var winningWordLength;
+  var highScoreWord = "";
 
   for (var i = 0; i < arrayOfWords.length; i++) {
     var word = arrayOfWords[i];
     var score = this.score(word);
-    var sevenBonus = (winningWordLength == 7);
+    var sevenBonus = (highScoreWord.length == 7);
     var tie = (score == highScore);
-    var tieWinner = (!sevenBonus && tie && (word.length == 7 || word.length < winningWordLength));
+    var tieWinner = (!sevenBonus && tie && (word.length == 7 || word.length < highScoreWord.length));
 
     if (score > highScore || tieWinner) {
       highScore = score;
       highScoreWord = word;
-      winningWordLength = word.length;
     }
 
-    // console.log("++++++++");
-    // console.log(word);
-    // console.log("score: " + score);
-    // console.log("high score: " + highScore);
-    // console.log("winning word: " + highScoreWord);
-    // console.log("winning word length: " + winningWordLength);
+    console.log("++++++++");
+    console.log(word);
+    console.log("score: " + score);
+    console.log("high score: " + highScore);
+    console.log("winning word: " + highScoreWord);
   }
   return highScoreWord;
 };
 
-// var highWord = Scrabble.highestScoreFrom(["aaa", "sss", "xxx", "qqaaaa", "xxxf", "jjjf", "qqj", "a", "zzzzzj", "aaaaaad"]);
-// console.log(highWord);
+var highWord = Scrabble.highestScoreFrom(["aaa", "sss", "xxx", "qqaaaa", "xxxf", "jjjf", "qqj", "a", "zzzzzj", "aaaaaad"]);
+console.log(highWord);
 
 var Player = function(name) {
   this.name = name;
@@ -53,6 +50,7 @@ var Player = function(name) {
 
 Player.prototype.play = function(word) {
   if (this.hasWon()) {
+    console.log("You can't play more - you've already won!");
     return false;
   }
   this.plays.push(word);
@@ -88,32 +86,32 @@ Player.prototype.highestWordScore = function() {
   return Scrabble.score(this.highestScoringWord());
 };
 
-addie = new Player("Addie");
-console.log(addie.highestScoringWord());
-console.log(addie.highestWordScore());
-addie.play("apple");
-console.log(addie.totalScore());
-addie.play("a");
-console.log(addie.totalScore());
-addie.play("q");
-console.log(addie.totalScore());
-console.log(addie.hasWon());
-addie.play("xxxxxx");
-console.log(addie.totalScore());
-addie.play("xxx");
-console.log(addie.totalScore());
-console.log(addie.hasWon());
-addie.play("j");
-console.log(addie.totalScore());
-console.log(addie.hasWon());
-addie.play("x");
-console.log(addie.totalScore());
-console.log(addie.hasWon());
-addie.play("qqaaaa");
-console.log(addie.highestScoringWord());
-console.log(addie.highestWordScore());
-console.log(addie.plays);
-console.log(addie.totalScore());
+// addie = new Player("Addie");
+// console.log(addie.highestScoringWord());
+// console.log(addie.highestWordScore());
+// addie.play("apple");
+// console.log(addie.totalScore());
+// addie.play("a");
+// console.log(addie.totalScore());
+// addie.play("q");
+// console.log(addie.totalScore());
+// console.log(addie.hasWon());
+// addie.play("xxxxxx");
+// console.log(addie.totalScore());
+// addie.play("xxx");
+// console.log(addie.totalScore());
+// console.log(addie.hasWon());
+// addie.play("j");
+// console.log(addie.totalScore());
+// console.log(addie.hasWon());
+// addie.play("x");
+// console.log(addie.totalScore());
+// console.log(addie.hasWon());
+// addie.play("qqaaaa");
+// console.log(addie.highestScoringWord());
+// console.log(addie.highestWordScore());
+// console.log(addie.plays);
+// console.log(addie.totalScore());
 
 var TileBag = function() {
   var bag = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "i", "j", "k", "l", "l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z"];
