65eb9ef6515a9a976df7253113beaab20ba941c4
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-16 20:06:44 -0700

    function is almost complete, need to write cases for ties

--- scrabble.js
+++ scrabble.js
@@ -23,38 +23,78 @@ populateScoreboard(["q", "z"], 10);
 
 
 
-var Scrabble = function(word) {
-  this.word = word;
+var Scrabble = function() { // this is basically a class
 };
 
 Scrabble.prototype = {
-  score: function() {
+  score: function(word) {
     var word_score = 0;
     // for each letter in the word, add points to word_score
-    for (var index = 0; index < this.word.length; index++) { // index is our place in the word
-      var letter = this.word[index]; // this grabs the letter from the word (e.g. "y" from "you")
+    for (var index = 0; index < word.length; index++) { // index is our place in the word
+      var letter = word[index]; // this grabs the letter from the word (e.g. "y" from "you")
       word_score += scoreboard[letter];
     }
     // check length of word to see if they get a bonus
-    if (this.word.length == 7) {
+    if (word.length == 7) {
       word_score += 50;
     }
-    // print total word score
-    console.log(word_score);
+    // return total word score
+    return word_score;
+  },
+  highestScore: function(arrayOfWords) {
+    var all_words_played = {};
+    // add all words played AND their score to object
+    for (var index = 0; index < arrayOfWords.length; index++) {
+      var word = arrayOfWords[index];
+      all_words_played[word] = this.score(word);
+    }
+    var highest_scores = [];
+    var max_score = 0;
+    // search object for highest score
+      for (var word in all_words_played) {
+        var score = all_words_played[word]; // keys are words
+        if (score > max_score) {
+          max_score = score;
+          highest_scores = [word];
+        } else if (score == max_score) {
+          highest_scores.append(word);
+        }
+      }
+    // if two values are the same (tie)
+    if (highest_scores.length > 1) {
+      var wordLengths = {};
+      for (var index = 0; index < highest_scores.length; index++) {
+        var word = highest_scores[index]
+        wordLengths[word.length] = 1; // we are only using the object for the keys, not the values
+        // if words are the same length
+
+        // return the first played
+      }
+      if (wordLengths.length == 1)
+      // if one of them has 7 letters
+      if (word.length == 7) {
+        // return that word as the winner
+        return word;
+      }
+
+    }
+    //return winning word
+    return highest_scores[0];
   }
 };
 
-var calculate = function(error, input) {
-  var word = new Scrabble(input.word);
-  word.score();
-};
+// var calculate = function(error, input) {
+//   var play = new Scrabble();
+//   play.score(input.word);
+// };
+//
+// prompt.get(["word"], calculate);
 
-prompt.get(["word"], calculate);
 
 
-
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
-};
-
-module.exports = Scrabble;
+// code they gave us:
+// Scrabble.prototype.helloWorld = function() {
+//   return 'hello world!';
+// };
+//
+// module.exports = Scrabble;
