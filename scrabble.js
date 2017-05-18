69e20faddd5cde2992173265e8d0597066b50cc7
add2point71dots <alcland@gmail.com>
2017-05-17 21:10:36 -0700

    removed all my testing console.logs

--- scrabble.js
+++ scrabble.js
@@ -30,18 +30,9 @@ Scrabble.highestScoreFrom = function(arrayOfWords) {
       highScore = score;
       highScoreWord = word;
     }
-
-    console.log("++++++++");
-    console.log(word);
-    console.log("score: " + score);
-    console.log("high score: " + highScore);
-    console.log("winning word: " + highScoreWord);
   }
   return highScoreWord;
 };
-
-var highWord = Scrabble.highestScoreFrom(["aaa", "sss", "xxx", "qqaaaa", "xxxf", "jjjf", "qqj", "a", "zzzzzj", "aaaaaad"]);
-console.log(highWord);
 
 var Player = function(name) {
   this.name = name;
@@ -66,7 +57,7 @@ Player.prototype.totalScore = function() {
 };
 
 Player.prototype.hasWon = function() {
-  return (this.totalScore() > 100) ? true : false;
+  return (this.totalScore() > 100);
 };
 
 Player.prototype.highestScoringWord = function() {
@@ -85,33 +76,6 @@ Player.prototype.highestWordScore = function() {
   }
   return Scrabble.score(this.highestScoringWord());
 };
-
-// addie = new Player("Addie");
-// console.log(addie.highestScoringWord());
-// console.log(addie.highestWordScore());
-// addie.play("apple");
-// console.log(addie.totalScore());
-// addie.play("a");
-// console.log(addie.totalScore());
-// addie.play("q");
-// console.log(addie.totalScore());
-// console.log(addie.hasWon());
-// addie.play("xxxxxx");
-// console.log(addie.totalScore());
-// addie.play("xxx");
-// console.log(addie.totalScore());
-// console.log(addie.hasWon());
-// addie.play("j");
-// console.log(addie.totalScore());
-// console.log(addie.hasWon());
-// addie.play("x");
-// console.log(addie.totalScore());
-// console.log(addie.hasWon());
-// addie.play("qqaaaa");
-// console.log(addie.highestScoringWord());
-// console.log(addie.highestWordScore());
-// console.log(addie.plays);
-// console.log(addie.totalScore());
 
 var TileBag = function() {
   var bag = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "i", "j", "k", "l", "l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z"];
@@ -154,25 +118,3 @@ TileBag.prototype.drawTiles = function(num) {
 TileBag.prototype.tilesRemaining = function(num) {
   return this.tiles.length;
 };
-
-// bag = new TileBag();
-// console.log(bag.tilesRemaining());
-// console.log(bag.drawTiles(-1));
-// console.log(bag.drawTiles(9));
-// console.log(bag.drawTiles(7));
-// console.log(bag.tilesRemaining());
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
-// console.log(bag.drawTiles(7));
