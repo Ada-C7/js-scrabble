afe82d21ef74cc30c6975529c3decb35b621e865
Brenna <bcmdarroch@gmail.com>
2017-05-17 17:22:07 -0700

    finalized

--- scrabble.js
+++ scrabble.js
@@ -1,7 +1,8 @@
 var Scrabble = function() {
   this.letterValues = { "a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10
-};
-this.tilebag = new Tilebag();
+  };
+  this.tilebag = new Tilebag();
+  this.dictionary = new Dictionary();
 };
 
 // also works: define Scrabble.score, etc
@@ -143,6 +144,14 @@ Tilebag.prototype = {
   }
 };
 
+// var Dictionary = function() {
+//   this.allWords = ;
+// };
+//
+// Dictionary.prototype = {
+//   validWord: function(num) {
+//   }
+// };
 
 // tests
 var myScrabble = new Scrabble();
