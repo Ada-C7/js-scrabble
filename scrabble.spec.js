09047fc357050e3999c1d9b275c154e3561e699f
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-18 07:06:33 -0700

    organizes tests by function

--- scrabble.spec.js
+++ scrabble.spec.js
@@ -2,100 +2,116 @@ var Scrabble = require('./scrabble');
 var Player = require('./player');
 
 describe('Scrabble Test Suite', function () {
-  it('scores a word correctly', function () {
-    var scrabble = new Scrabble();
 
-    expect(scrabble.score('sandwich')).toEqual(17);
+  describe('Scrabble score', function () {
+    it('scores a word correctly', function () {
+      var scrabble = new Scrabble();
+
+      expect(scrabble.score('sandwich')).toEqual(17);
+    });
+
+    it('gives 50 bonus points to 7 letter words', function () {
+      var scrabble = new Scrabble();
+
+      expect(scrabble.score('aaaaaaa')).toEqual(57);
+    });
   });
 
-  it('gives 50 bonus points to 7 letter words', function () {
-    var scrabble = new Scrabble();
+  describe('Scrabble highestScoreFrom', function () {
+    it('chooses the higher scoring word', function () {
+      var scrabble = new Scrabble();
 
-    expect(scrabble.score('aaaaaaa')).toEqual(57);
+      expect(scrabble.highestScoreFrom(['sandwich', 'bacon'])).toEqual('sandwich');
+    });
+
+    it('chooses the shorter word when there is a tie', function () {
+      var scrabble = new Scrabble();
+
+      expect(scrabble.highestScoreFrom(['w', 'aaaa'])).toEqual('w');
+    });
+
+    it('chooses the word with 7 letters if there is a tie', function () {
+      var scrabble = new Scrabble();
+
+      expect(scrabble.highestScoreFrom(['aaaaaaf', 'qqqqqq'])).toEqual('aaaaaaf');
+    });
+
+    it('chooses the first 7 letter word if there are multiple', function () {
+      var scrabble = new Scrabble();
+
+      expect(scrabble.highestScoreFrom(['aaaaaaa', 'eeeeeee']));
+    });
   });
 
-  it('chooses the higher scoring word', function () {
-    var scrabble = new Scrabble();
+  describe('Player', function () {
+    it('sets up a player with a name and a game', function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
 
-    expect(scrabble.highestScoreFrom(['sandwich', 'bacon'])).toEqual('sandwich');
+      expect(player1.name).toEqual('Alix');
+    });
   });
 
-  it('chooses the shorter word when there is a tie', function () {
-    var scrabble = new Scrabble();
+  describe('Player play', function () {
+    it('allows a player to play a word', function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
 
-    expect(scrabble.highestScoreFrom(['w', 'aaaa'])).toEqual('w');
+      player1.play('bacon');
+      expect(player1.plays).toEqual(['bacon']);
+    });
+
+    it("must return false if a player plays a word after winning", function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
+
+      player1.play('qqqqqqq'); // 120 points
+      expect(player1.play('ham')).toEqual(false);
+      expect(player1.totalScore()).toEqual(120);
+    });
   });
 
-  it('chooses the word with 7 letters if there is a tie', function () {
-    var scrabble = new Scrabble();
+  describe('Player totalScore', function () {
+    it("correctly calculates a player's total score", function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
 
-    expect(scrabble.highestScoreFrom(['aaaaaaf', 'qqqqqq'])).toEqual('aaaaaaf');
+      player1.play('bacon'); // 9 points
+      player1.play('ham'); // 8 points
+      expect(player1.totalScore()).toEqual(17);
+    });
   });
 
-  it('chooses the first 7 letter word if there are multiple', function () {
-    var scrabble = new Scrabble();
+  describe('Player hasWon', function () {
+    it("shows that a player with over 100 points has won", function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
 
-    expect(scrabble.highestScoreFrom(['aaaaaaa', 'eeeeeee']));
+      player1.play('qqqqqqq'); // 120 points
+      expect(player1.totalScore()).toEqual(120);
+      expect(player1.hasWon()).toEqual(true);
+    });
+
+    it("shows that a player with under 100 points has not won", function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
+
+      player1.play('ham'); // 8 points
+      expect(player1.totalScore()).toEqual(8);
+      expect(player1.hasWon()).toEqual(false);
+    });
   });
 
-  it('sets up a player with a name and a game', function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
+  describe('Player highestScoringWord and highestWordScore', function () {
+    it("returns a player's highest scoring word", function () {
+      var scrabble = new Scrabble();
+      var player1 = new Player('Alix', scrabble);
 
-    expect(player1.name).toEqual('Alix');
+      player1.play('bacon'); // 9 points
+      player1.play('ham'); // 8 points
+      expect(player1.highestScoringWord()).toEqual('bacon');
+      expect(player1.highestWordScore()).toEqual(9);
+    });
   });
-
-  it('allows a player to play a word', function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('bacon');
-    expect(player1.plays).toEqual(['bacon']);
-  });
-
-  it("correctly calculates a player's total score", function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('bacon'); // 9 points
-    player1.play('ham'); // 8 points
-    expect(player1.totalScore()).toEqual(17);
-  });
-
-  it("shows that a player with over 100 points has won", function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('qqqqqqq'); // 120 points
-    expect(player1.totalScore()).toEqual(120);
-    expect(player1.hasWon()).toEqual(true);
-  });
-
-  it("shows that a player with under 100 points has not won", function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('ham'); // 8 points
-    expect(player1.totalScore()).toEqual(8);
-    expect(player1.hasWon()).toEqual(false);
-  });
-
-  it("must return false if a player plays a word after winning", function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('qqqqqqq'); // 120 points
-    expect(player1.play('ham')).toEqual(false);
-    expect(player1.totalScore()).toEqual(120);
-  });
-
-  it("returns a player's highest scoring word", function () {
-    var scrabble = new Scrabble();
-    var player1 = new Player('Alix', scrabble);
-
-    player1.play('bacon'); // 9 points
-    player1.play('ham'); // 8 points
-    expect(player1.highestScoringWord()).toEqual('bacon');
-    expect(player1.highestWordScore()).toEqual(9);
-  });
+  
 });
