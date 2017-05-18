9437712d591540113f6637daa719fc7729f1703d
Kayla <kubicke.kayla@gmail.com>
2017-05-17 23:06:55 -0700

    Player can add word to word collection array.
    Added prototype function that adds an individual word to
    word collection array.
    Removed notes and cleaned up scrabble.js.

--- player.js
+++ player.js
@@ -1,7 +1,19 @@
-var Scrabble = require("scrabble");
+var Scrabble = require("./scrabble");
+
 
 var Player = function(name) {
   this.name = name;
   this.plays = [];
   this.scrabble = new Scrabble();
 };
+
+Player.prototype = {
+  play: function(word) {
+    // ADD BOOL LOGIC FROM hasWon()
+    this.plays.push(word);
+  }
+
+};
+
+player1 = new Player("Kerry");
+// player1.play("word");
