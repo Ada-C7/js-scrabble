7027304bafd54296831db82a8363e63c48fa9f2e
Sahana Murthy <s.murthy730.com>
2017-05-18 09:45:20 -0700

    Wave 2 completed

--- player.js
+++ player.js
@@ -17,8 +17,8 @@ Player.prototype.play = function(word) {
 // Function which sums up and returns the score of the players words
 Player.prototype.totalScore = function() {
   var sum = 0;
-  this.plays.forEach(function(play) {
-    sum += Scrabble.prototype.score(play);
+  this.plays.forEach(function(word) {
+    sum += Scrabble.prototype.score(word);
   });
   return sum;
 };
@@ -32,6 +32,18 @@ Player.prototype.hasWon = function() {
   };
 };
 
+// highestScoringWord(): Function which returns the highest scoring word the user has played
+Player.prototype.highestScoringWord = function() {
+  word = Scrabble.prototype.highestScoreFrom(this.plays);
+  return word;
+};
+
+// highestWordScore(): Function which returns the highestScoringWord score
+Player.prototype.highestWordScore = function() {
+  var word = this.highestScoringWord(this.plays);
+  var score = Scrabble.prototype.score(word);
+  return score;
+};
 
 var me = new Player("Sahana");
 console.log(me.plays);
@@ -39,3 +51,5 @@ console.log(me.play("hello"));
 console.log(me.plays);
 console.log(me.totalScore());
 console.log(me.hasWon());
+console.log(me.highestScoringWord());
+console.log(me.highestWordScore());
