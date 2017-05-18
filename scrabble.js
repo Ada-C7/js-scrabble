ff7743ff057ce7b2ad022cc579b86db1d68e61f6
Theresa Manney <theresa.m.17@gmail.com>
2017-05-17 21:43:53 -0700

    most of the player functionality is working, finishing up logic to print the highest scoring word and its score

--- scrabble.js
+++ scrabble.js
@@ -64,22 +64,80 @@ Scrabble.prototype.highestScore = function(arrayOfWords) {
   return winningWord;
 };
 // TESTS
-var testGame  = new Scrabble();
-
+// var testGame = new Scrabble();
+//
 // var wordsArray = ["Test", "buTTer", "caR"];
-var wordsArray = ["aaa", "eee"];
+// var wordsArray = ["aaa", "eee"];
 
 // var wordsArray = ["aaaaaaa"];
 // should return buTTerfly
 // console.log(testGame.score("adbfkjq"));
 // test should = 4
-console.log(testGame.highestScore(wordsArray));
+// console.log(testGame.highestScore(wordsArray));
 // butter should win... should return buTTer
 
 
 // PLAYER
+var Player = function(name, game) {
+  this.name = name;
+  this.plays = [];
+  game = new Scrabble();
+  this.scrabble = game;
 
+};
 
+Player.prototype.play = function(word) {
+  // Returns false if player has already won
+  if (this.hasWon()) {
+    return false;
+  } else {
+  // Function which adds the input word to the plays Array
+    this.plays.push(word);
+    return true;
+  }
+};
+
+Player.prototype.totalScore = function() {
+  // Function which sums up and returns the score of the players words
+  var total = 0;
+  for (var i = 0; i < this.plays.length; i++) {
+    total += this.scrabble.score(this.plays[i]);
+  }
+  return total;
+
+};
+
+Player.prototype.hasWon = function() {
+  // Function which sums up and returns the score of the players words
+  if (this.totalScore() > 100) {
+    return true;
+  } else {
+    return false;
+    }
+};
+
+Player.prototype.highestScoringWord = function() {
+  // Function which returns the highest scoring word the user has played
+  return this.scrabble.highestScore(this.plays);
+};
+
+Player.prototype.highestWordScore = function() {
+  // Function which returns the highestScoringWord score
+};
+
+// Game set up:
+var testPlayer = new Player("Theresa");
+
+testPlayer.play("flower"); // 12pts
+testPlayer.play("zzzz"); // 40pts
+
+// Display results:
+// Total points for the game
+console.log("Player: " + testPlayer.name + "'s total points: " + testPlayer.totalScore());
+// Has the player won
+console.log("Has player won? " + testPlayer.hasWon());
+// Highest Scoring word and its associated score
+console.log("Winning word: " );
 
 
 module.exports = Scrabble;
