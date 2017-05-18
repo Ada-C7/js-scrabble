4020ab04b9f627c65607e10695148445a7da7abb
Tamiko Terada <tamikodevo@gmail.com>
2017-05-18 00:01:07 -0700

    add play function to player.prototype

--- player.js
+++ player.js
@@ -5,17 +5,20 @@ var Player = function(name) {
   this.name = name;
   this.plays = [];
   this.score = 0;
+  this.won = false;
 };
 
 // PLAYER Prototype - function for Player to play(word)
 Player.prototype.play = function(word) {
-  Scrabble.scoreWord.call(this, word);
+  var single_score = Scrabble.prototype.scoreWord(word);
+  this.score += single_score;
+  return single_score;
 };
 
 // PLAYER Prototype - function to return totalScore()
-// Player.prototype.totalScore = function() {
-// ;
-// }
+Player.prototype.totalScore = function() {
+  return this.score;
+}; //funtion not necessary? .score does the same thing
 
 // PLAYER Prototype - function to show win (T/F)
 // Player.prototype.hasWon = function() {
@@ -32,13 +35,12 @@ Player.prototype.play = function(word) {
 // returns a score
 // }
 
-
 // print out single player data
-var player1 = my_game.addPlayer("Tamiko");
-var player2 = my_game.addPlayer("Tofu");
-console.log(my_game.players);
+var tamiko = new Player("Tamiko");
 
-console.log(my_game.scoreWord.call(player1, "aaa"));
+console.log(tamiko.name);
+console.log(tamiko.play("zzz"));
+console.log(tamiko.totalScore());
 
 // console.log(player1.name);
 // console.log(player1.plays);
