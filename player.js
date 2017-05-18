57e33c7d1b4c1bd36e34c9c2600ef48b598b6fa0
Kayla <kubicke.kayla@gmail.com>
2017-05-18 00:06:06 -0700

    Wrote skeleton for totalScore.

--- player.js
+++ player.js
@@ -11,9 +11,39 @@ Player.prototype = {
   play: function(word) {
     // ADD BOOL LOGIC FROM hasWon()
     this.plays.push(word);
-  }
+    return this.plays;
+  },
 
+  totalScore: function() {
+    // var wordScore = 0;
+    // var total = 0;
+    //
+    // this.play("word");
+    // //this.plays is undefined
+    // this.plays.forEach(function(word){
+    //   console.log(this.plays);
+    //   // score = this.scrabble.score(word);
+    //   wordScore = this.scrabble.score(word);
+    //   total += wordScore;
+    // });
+    // return total;
+  },
+
+  hasWon: function() {
+
+  },
+
+  highestScoringWord: function() {
+
+  },
+
+  highestWordScore: function() {
+    
+  }
 };
 
 player1 = new Player("Kerry");
-// player1.play("word");
+player1.play("word");
+// player1.totalScore();
+
+// array = ["word", "dog"];
