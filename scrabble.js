283d308b041d058c4d32516f0cf8abcdb90a69d2
Tamiko Terada <tamikodevo@gmail.com>
2017-05-17 23:07:53 -0700

    use constructor.call method to call Player through Scrabble

--- scrabble.js
+++ scrabble.js
@@ -1,79 +1,80 @@
 // SCRABBLE Constructor
-var Scrabble = function() {
-};
-
-// SCRABBLE Prototype - function to score a single word
-Scrabble.prototype.score = function(word) {
-
-  var scoring_hash = {
-    aeioulnrst: 1,
-    dg: 2,
-    bcmp: 3,
-    fhvwy: 4,
-    k: 5,
-    kx: 8,
-    qz: 10,
-  };
-
-  // add 50 pts if word length is 7
-  var score = 0;
-  if (word.length === 7) {
-    score += 50;
-  }
-
-  // iterate over each char of input word
-  // Q: define i in the first time only??
-  for (var i = 0; i < word.length; i++) {
-    var letter = word[i];
-    // iterate over each key of hash
-    for (var key in scoring_hash) {
-      if (key.includes(letter)) {
-        score += scoring_hash[key];
-      }
-    }
-  }
-  return score;
-};
-
-// SCRABBLE Prototype - function to find max score word in array
-Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-
-  var high_score = this.score(arrayOfWords[0]);
-
-  // iterate over array of input words to find max score
-  for (var i = 0; i < arrayOfWords.length; i++) {
-    var word_score = this.score(arrayOfWords[i]);
-    if ( word_score > high_score) {
-      high_score = word_score;
-    }
-  }
-  // add words with high_score into an array
-  var max_words = []; // to hold words with max points
-  for (i = 0; i < arrayOfWords.length; i++) {
-    if (this.score(arrayOfWords[i]) === high_score) {
-      max_words.push(arrayOfWords[i]);
-    }
-  }
-  // return the word with shortest length
-  var shortest_word = max_words[0];
-  for (i = 0; i < max_words.length; i++) {
-    if (max_words[i].length < shortest_word.length) {
-      shortest_word = max_words[i];
-    }
-  }
-  return shortest_word;
+var Scrabble = function(){
+  this.players = [];
 };
 
 // PLAYER Constructor
 var Player = function(name) {
   this.name = name;
-  this.plays = new Array; // returns all words played
+  this.plays = [];
+  this.score = 0;
+};
+
+// SCRABBLE Prototype
+Scrabble.prototype = {
+  //- FUNCTION to add a new player
+  addPlayer: function(name) {
+    this.players.push(new Player(name));
+  },
+
+  //- FUNCTION to score a single word
+  scoreWord: function(word) {
+    var scoring_hash = {
+      aeioulnrst: 1,
+      dg: 2,
+      bcmp: 3,
+      fhvwy: 4,
+      k: 5,
+      kx: 8,
+      qz: 10,
+    };
+
+    var score = 0;
+    // add 50 pts if word length is 7
+    if (word.length === 7) score += 50;
+
+    // iterate over each char of input word -- Q: ok to var i 1st time only??
+    for (var i = 0; i < word.length; i++) {
+      var letter = word[i];
+      // iterate over each key of hash
+      for (var key in scoring_hash) {
+        if (key.includes(letter)) score += scoring_hash[key];
+      }
+    }
+    return score;
+  },
+
+  //- FUNCTION to find max score word in array
+  highestScoreFrom: function(arrayOfWords) {
+    var high_score = this.scoreWord(arrayOfWords[0]);
+
+    // iterate over array of input words to find max score
+    for (var i = 0; i < arrayOfWords.length; i++) {
+      var word_score = this.scoreWord(arrayOfWords[i]);
+      if ( word_score > high_score) high_score = word_score;
+    }
+    // add words with high_score into an array
+    var max_words = []; // to hold words with max points
+    for (i = 0; i < arrayOfWords.length; i++) {
+      if (this.scoreWord(arrayOfWords[i]) === high_score) {
+        max_words.push(arrayOfWords[i]);
+      }
+    }
+    // return the word with shortest length
+    var shortest_word = max_words[0];
+    for (i = 0; i < max_words.length; i++) {
+      if (max_words[i].length < shortest_word.length) {
+        shortest_word = max_words[i];
+      }
+    }
+    return shortest_word;
+  },
 };
 
 // PLAYER Prototype - function for Player to play(word)
-// Player.prototype.play = function(word) {
-// ;
-// }
+Player.prototype.play = function(word) {
+  Scrabble.scoreWord.call(this, word);
+};
 
 // PLAYER Prototype - function to return totalScore()
 // Player.prototype.totalScore = function() {
@@ -108,10 +109,23 @@ module.exports = Scrabble; // starter code
 var my_game = new Scrabble();
 
 // print out results of single word score
-console.log(my_game.score("zzz"));
+console.log(my_game.scoreWord("zzz"));
+console.log(my_game.scoreWord("aaaaaaa"));
 console.log(my_game.highestScoreFrom(["aaa", "zzj", "zzx"]));
 
 // print out single player data
-var tamiko = new Player("Tamiko");
-console.log(tamiko.name);
-console.log(tamiko.plays);
+var player1 = my_game.addPlayer("Tamiko");
+var player2 = my_game.addPlayer("Tofu");
+console.log(my_game.players);
+
+console.log(my_game.scoreWord.call(player1, "aaa"));
+
+// console.log(player1.name);
+// console.log(player1.plays);
+// console.log(player1.play("aaa"));
+
+
+// console.log(Object.getPrototypeOf(Scrabble));
+// console.log(tamiko.score);
+
+// console.log(tamiko.plays);
