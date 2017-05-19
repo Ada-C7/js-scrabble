49e078bce4980c4b4b8358ee1a8de186ec37655e
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-19 09:36:35 -0700

    tried debugging last requirement

--- player.js
+++ player.js
@@ -12,11 +12,20 @@ Player.prototype = {
     return this.plays
   },
 
+  // totalScore: function() {
+  //   var allScore = 0;
+  //   for (var i = 0; i < this.plays.length; i++){
+  //     allScore += this.game.score(this.plays[i])
+  //   }
+  //   return allScore
+  // },
+
   totalScore: function() {
     var allScore = 0;
+
     this.plays.forEach(function(word){
       allScore += this.game.score(word)
-    })
+    }.bind(this))
     return allScore
   },
   hasWon: function() {
@@ -29,24 +38,33 @@ Player.prototype = {
   },
   // Function which returns the highest scoring word the user has played
   highScoringWord: function() {
-    var highestWord = this.game.highestScoreFrom(this.plays);
-    return highestWord
+    var highestWord = Scrabble.prototype.highestScoreFrom(this.plays);
+    return highestWord;
   },
   //  Function which returns the highestScoringWord score
   highestWordScore: function (){
-    var highestWord = this.highScoringWord()
-    var highScore = this.game.score(highestWord)
-    return highScore
+    var highestWord = this.highScoringWord(this.plays);
+    var highScore = Scrabble.prototype.score(highestWord);
+    // return highScore;
+    return highestWord
   }
-  //end
 };
 
 var Scrabble = require("./scrabble") // do this here to pass in an instance of scrabble every time a player is created
 var firstGame = new Player("Marisol", new Scrabble())
+
 console.log(firstGame.play("apple"))
+console.log(firstGame.play("zzzzzzz"))
 console.log(firstGame.play("notebook"))
 console.log(firstGame.play("google"))
+console.log(firstGame.play("marisol"))
+
 
 console.log(firstGame.totalScore())
+console.log(firstGame.hasWon())
+
+console.log(firstGame.plays)
+console.log(firstGame.highScoringWord())
+//console.log(firstGame.highestWordScore()) // could not get this to work because receiving error about changing case in scrabble file.
 
 // module.exports = Player;
--- scrabble.js
+++ scrabble.js
@@ -2,40 +2,49 @@
 var Scrabble = function() {};
 var scoreChart = {
   "a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "x": 8, "y": 4, "z": 10
-}
+};
 
 Scrabble.prototype.score = function(word) {
+  //accounting for if user input is varied in upper and lower case.
+  word = word.toLowerCase();
   //proud of this ternary that is assigning scoreOfWord based on whether word used all 7 tiles to get 50 point bonus
   var scoreOfWord = word.length == 7? 50: 0;
-  //accounting for if user input is varied in upper and lower case.
-  // word = word.toLowerCase();
+
+
   //loop that is iterating through word letter by letter and comparing it to scoreChart object
-  for (var i = 0; i < word.length; i++)
-  scoreOfWord += scoreChart[word.charAt(i)];
+  for (var i = 0; i < word.length; i++){
+    scoreOfWord += scoreChart[word.charAt(i)];
+  }
   return scoreOfWord
-  scoreOfWord = 0;
 };
 
 // collection of any word that has the current highest score as you're iterating through
 Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-  var highScoringWord = this.score(arrayOfWords[0]);
+  var highestScore = this.score(arrayOfWords[0]);
   for (var i = 0; i < arrayOfWords.length; i++) {
-    var wordScore = this.score(arrayOfWords[i]);
-    if ( wordScore > highScoringWord) {
-      highScoringWord = wordScore;
-      return highScoringWord
+    var currentWordScore = this.score(arrayOfWords[i]);
+    if ( currentWordScore > highestScore) {
+      highestScore = currentWordScore;
+      return highestScore
     }
   };
 
   // return collection of words with tieing highest scores
   var collectionHighestScoringWords = [];
   for (var i = 0; i < arrayOfWords.length; i++) {
-    if (this.score(arrayOfWords[i]) == highScoringWord) {
+    if (this.score(arrayOfWords[i]) == highestScore) {
       collectionHighestScoringWords.push(arrayOfWords[i])
       return collectionHighestScoringWords
     }
   };
 
+//i tried writing a function that would return the winning word if there was a single winning word but it didn't work.
+  // var singleWinningWord = function (){
+  //   if (collectionHighestScoringWords.length == 1){
+  //     return collectionHighestScoringWords[0]
+  //   }
+  // };
+  
   // return word with shortest length if multiple words tie for score
   var selectShortestWord = collectionHighestScoringWords[0];
   for (var i = 0; i < collectionHighestScoringWords.length; i++){
@@ -50,7 +59,7 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
   if (collectionHighestScoringWords.length > 1){
     return collectionHighestScoringWords[0]
   };
-//end of highestScoreFrom function
+  //end of highestScoreFrom function
 }
 
 
@@ -61,8 +70,9 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
 //
 module.exports = Scrabble;
 
-// var player1 = new Scrabble();
-// var player2 = new Scrabble();
+var player1 = new Scrabble();
+var player2 = new Scrabble();
 // console.log("Player 1 played: 'Marisol', and received " + player1.score("MaRiSoL") + " points.")
-// console.log("Player 1's highest scoring word was: " + player1.highestScoreFrom([ "QqQq", "zZzZ"]))
+// console.log("Player 1 played: 'Marisol', and received " + player1.score("apple") + " points.")
+// console.log("Player 1's highest scoring word was: " + player1.highestScoreFrom([ "qqqq", "zzzz", "apple"]))
 // console.log("Player 2's highest scoring word was: " + player2.highestScoreFrom([ "zZzZ", "QqQq"]))
