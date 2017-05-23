0de39392b9e5f8d4db572f3292e2562e82a0d0ae
Kayla <kubicke.kayla@gmail.com>
2017-05-23 00:11:44 -0700

    Refactored code based on Noam's suggestions.
    Removed sort loop from highestScoreFrom as it was unnecessary.
    Removed initial value from reduce loop in highestScoreFrom because it will grab the first element as it's initial value.
    Added rules and changed variable names to increase readability.
    Cleaned up logic.
    Fixed highestScoreFrom bug; first 7 letter word will be returned.

--- scrabble.js
+++ scrabble.js
@@ -7,19 +7,19 @@ Scrabble.prototype = {
 
     var total = wordArray.reduce(function(acc, letter) {
 
-      if (letter.match(/[a|e|i|o|u|l|n|r|s|t]/i)) {
+      if (letter.match(/[a|e|i|o|u|l|n|r|s|t]/)) {
         return acc + 1;
-      } else if (letter.match(/[d|g]/i)) {
+      } else if (letter.match(/[d|g]/)) {
         return acc + 2;
-      } else if (letter.match(/[b|c|m|p]/i)) {
+      } else if (letter.match(/[b|c|m|p]/)) {
         return acc + 3;
-      } else if (letter.match(/[f|h|v|w|y]/i)) {
+      } else if (letter.match(/[f|h|v|w|y]/)) {
         return acc + 4;
-      } else if (letter.match(/[k]/i)) {
+      } else if (letter.match(/[k]/)) {
         return acc + 5;
-      } else if (letter.match(/[j|x]/i)) {
+      } else if (letter.match(/[j|x]/)) {
         return acc + 8;
-      } else if (letter.match(/[q|z]/i)) {
+      } else if (letter.match(/[q|z]/)) {
         return acc + 10;
       } else {
         throw new Error("Invalid word.");
@@ -33,23 +33,31 @@ Scrabble.prototype = {
   },
 
   highestScoreFrom: function(arrayOfWords) {
-    var wordInfo = [];
 
-    arrayOfWords.forEach(function(word) {
-      wordInfo.push([word, this.score(word)]);
+    var wordInfo = arrayOfWords.map(function(word) {
+      return [word, this.score(word)];
     }.bind(this));
 
-    sortedWords = wordInfo.sort(function(scoreOne, scoreTwo) {
-      return scoreOne[1] - scoreTwo[1];
-    });
+    // Rules: If the top score is tied between multiple words,
+    // pick the one with the fewest letters.
+    // If the top score is tied between multiple words, pick
+    // the one with the fewest letters.
+    // If the there are multiple words that are the same score
+    // and same length, pick the first one in supplied list.
 
-    var highestWord = sortedWords.reduce(function(acc, pair) {
-      if (pair[1] > acc[1] || (pair[1] >= acc[1] && pair[0].length == 7)) {
-        return pair;
+    var highestScoringWord = wordInfo.reduce(function(bestWordSoFar, currentWord) {
+
+      if (currentWord[1] > bestWordSoFar[1]) {
+        return currentWord;
       }
-      return acc;
-    }, ["", 0]);
-    return highestWord[0];
+      if (currentWord[0].length === 7 && bestWordSoFar[0].length < 7) {
+        if (currentWord[1] === bestWordSoFar[1]) {
+          return currentWord;
+        }
+      }
+      return bestWordSoFar;
+    });
+    return highestScoringWord[0];
   }
 };
 
