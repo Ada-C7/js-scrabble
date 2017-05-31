205de745d66c2462adeeddb1c3b0f8f524e2b7ff
Kayla <kubicke.kayla@gmail.com>
2017-05-31 11:21:18 -0700

    Fixed two bugs.
    Reintroduced 'i' into regular expressions based on Noam's suggestions. (I misunderstood their purpose.)
    First shortest word with the highest score is returned in cases where the winning word is not seven characters long and multiple words have the same length and score.

--- scrabble.js
+++ scrabble.js
@@ -7,19 +7,19 @@ Scrabble.prototype = {
 
     var total = wordArray.reduce(function(acc, letter) {
 
-      if (letter.match(/[a|e|i|o|u|l|n|r|s|t]/)) {
+      if (letter.match(/[a|e|i|o|u|l|n|r|s|t]/i)) {
         return acc + 1;
-      } else if (letter.match(/[d|g]/)) {
+      } else if (letter.match(/[d|g]/i)) {
         return acc + 2;
-      } else if (letter.match(/[b|c|m|p]/)) {
+      } else if (letter.match(/[b|c|m|p]/i)) {
         return acc + 3;
-      } else if (letter.match(/[f|h|v|w|y]/)) {
+      } else if (letter.match(/[f|h|v|w|y]/i)) {
         return acc + 4;
-      } else if (letter.match(/[k]/)) {
+      } else if (letter.match(/[k]/i)) {
         return acc + 5;
-      } else if (letter.match(/[j|x]/)) {
+      } else if (letter.match(/[j|x]/i)) {
         return acc + 8;
-      } else if (letter.match(/[q|z]/)) {
+      } else if (letter.match(/[q|z]/i)) {
         return acc + 10;
       } else {
         throw new Error("Invalid word.");
@@ -47,7 +47,9 @@ Scrabble.prototype = {
 
     var highestScoringWord = wordInfo.reduce(function(bestWordSoFar, currentWord) {
 
-      if (currentWord[1] > bestWordSoFar[1]) {
+      if (currentWord[1] === bestWordSoFar[1] && currentWord[0].length < bestWordSoFar[0].length) {
+        return currentWord;
+      } else if (currentWord[1] > bestWordSoFar[1]) {
         return currentWord;
       }
       if (currentWord[0].length === 7 && bestWordSoFar[0].length < 7) {
